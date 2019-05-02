c244881623a768f54f6e26b05d2568d33c58d65c
and-gorbik <and-gorbik@mail.ru>
2019-05-02 23:06:21 +0300

    Add additional info about file

--- pstree.pl
+++ pstree.pl
@@ -1,9 +1,17 @@
 use 5.016;
-use warnings;
+BEGIN{
+	if ($] < 5.018) {
+		package experimental;
+		use warnings::register;
+	}
+}
+no warnings 'experimental';
+
+# формат вывода
+my $fmt = "p";
 
 sub get_ppid {
 	my $path = shift;
-	$path .= "/status";
 	open my($fd), $path or die "Can't read $path\n";
 	my @lines = <$fd>;
 	close $fd or die "Can't close $path\n";
@@ -12,31 +20,47 @@ sub get_ppid {
 	return $lines[1];
 }
 
+sub print_stat {
+	my $pid = shift;
+	my $fmt = shift;
+
+	open my($fd), "/proc/$pid/status" or die "Can't read /proc/$pid/status\n";
+	my @lines = <$fd>;
+	close $fd or die "Can't close /proc/$pid/status\n";
+	my %stat;
+	for (qw(0 7 8)) {
+		my @lst = split "\t", $lines[$_];
+		$stat{$lst[0]} = $lst[1];
+		chomp $stat{$lst[0]};
+	}
+
+	print "[$pid]" if $fmt =~ /p/;
+    print " $stat{'Name:'}" if $fmt =~ /n/;
+    print " uid=$stat{'Uid:'}" if $fmt =~ /u/;
+	print " gid=$stat{'Gid:'}" if $fmt =~ /g/;
+	print "\n";
+}
+
 opendir my($dir), "/proc" or die "Can't open /proc\n";
 my @lst = readdir $dir or die "Can't read from /proc\n";
 my %map;
 for (@lst) {
 	next unless /^\d+$/;
-	push @{$map{get_ppid("/proc/$_") + 0}}, $_ + 0;
+	push @{$map{get_ppid("/proc/$_/status") + 0}}, $_ + 0;
 }
 closedir $dir or die "Can't close /proc\n";
 
 sub traverse;
 sub traverse {
-	my $ppid = shift;
+	my $pid = shift;
 	my $depth = shift;
 	print "\t" for 1..$depth;
-	print "$ppid\n";
-	if (defined $map{"$ppid"}) {
-		for (@{$map{"$ppid"}}) {
-			traverse($_, $depth + 1);
-		}
+	print_stat $pid, $fmt;
+	for (@{$map{"$pid"}}) {
+		traverse($_, $depth + 1);
 	}
 }
 
-# my %args;
-# while (shift @ARGV) {
-# 	$args{$arg} if 
-# }
-traverse(0, 0);
+$fmt .= "nug";
+traverse(1, 0);
 
